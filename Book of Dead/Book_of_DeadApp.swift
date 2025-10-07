f86be02f85ab95496b63175ea028f3a2f74fe32c
Radcabna <radcabna@gmail.com>
2025-10-07 18:57:09 +0300

    Initial Commit

---
+++ Book of Dead/Book_of_DeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_of_DeadApp.swift
+//  Book of Dead
+//
+//  Created by Алкександр Степанов on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Book_of_DeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book of Dead/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Book of Dead
+//
+//  Created by Алкександр Степанов on 07.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
